1d214809adf63327344b3e890e00fb94d75b43d9
Romano Zumbé <rzumbe@gmail.com>
2022-12-05 21:37:59 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import const as keys
 import responses as R
 import json
+import asyncio
 from telegram.ext import *
 
 print("Bot started...")
@@ -16,7 +17,7 @@ def getReceivers_command(update, context):
 
 
 def help_command(update, context):
-    update.message.reply_text("Not implemented")
+    update.message.reply_text(f"Die bas URL ist {context.bot.base_url}")
 
 def addReceiver_command(update, context):
     msg = update.message
@@ -29,14 +30,15 @@ def handle_message(update, context):
 def handle_documents(update, context):
     msg = update.message
     print(f"Download of {msg.document.file_name} started")
-    msg.document.get_file().download_to_drive("c:/temp")
+    msg.document.get_file().download_to_drive()
     
 
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
 
 def main():
-    updater = Updater(keys.API_KEY, use_context=True)
+    updater = Updater(keys.API_KEY, use_context=True, base_url="http://192.168.43.53:8081/bot")
+    # updater.bot.logOut();
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start_command))
