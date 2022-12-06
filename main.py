03ef8841c364b10cf378c3b728a46d4dce6bbc01
Romano Zumbe <rzumbe@gmail.com>
2022-12-06 13:00:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def handle_documents(update: Update, context: CallbackContext):
     old_name = file.file_path
     new_name = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name
     os.rename(old_name, new_name)
-    bashCommand = f"unrar x '{new_name}'"
+    bashCommand = f"unrar x \"{new_name}\""
     print(bashCommand)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
