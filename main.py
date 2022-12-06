27843058b3d09466ace375369ff234707bc6f00f
Romano Zumbe <rzumbe@gmail.com>
2022-12-06 13:10:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def handle_documents(update: Update, context: CallbackContext):
     print(file)
     old_name = file.file_path
     new_name = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name
-    new_name_esc = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name.replace(" ", "\\") 
+    new_name_esc = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name.replace(" ", "\\ ") 
     os.rename(old_name, new_name)
     bashCommand = f"unrar x {new_name_esc}"
     print(bashCommand)
