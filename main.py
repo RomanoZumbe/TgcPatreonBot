b6663ccfe426123669a4ddc70c2f3bacc70a3622
Romano Zumbe <rzumbe@gmail.com>
2023-05-04 08:44:16 +0200

    Bla

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ from telegram import Update
 from telegram.ext import *
 import os
 import subprocess
+import re
 
 print("Bot started...")
+# messageReceivers = ["713224638","1794172761","5571326730"]
 messageReceivers = []
 
-# def getReceivers_command(update, context):
-#     for receiver in messageReceivers:
-#         update.message.reply_text(f"{receiver.first_name} {receiver.last_name} - {receiver.username} - {receiver.id}")
-#
-# def help_command(update, context):
-#     update.message.reply_text(f"Die bas URL ist {context.bot.base_url}")
-#
-# def addReceiver_command(update, context):
-#     msg = update.message
-#     messageReceivers.append(msg.from_user)
-#
-# def handle_message(update, context):
-#     for receiver in messageReceivers:
-#         update.message.copy(receiver.id)
+async def getReceivers_command(update, context):
+    for receiver in messageReceivers:
+        await update.message.reply_text(f"{receiver.first_name} {receiver.last_name} - {receiver.username} - {receiver.id}")
 
+async def help_command(update, context):
+    await update.message.reply_text(f"Die bas URL ist {context.bot.base_url}")
+
+async def addReceiver_command(update, context):
+    msg = update.message
+    messageReceivers.append(msg.from_user)
+    #print(msg)
+
+async def handle_message(update, context):
+    for receiver in messageReceivers:
+        await update.message.copy(receiver)
+
+"""
 async def handle_documents(update: Update, context: CallbackContext):
     msg = update.message
     # print(msg)
@@ -32,16 +36,31 @@ async def handle_documents(update: Update, context: CallbackContext):
     file = await msg.document.get_file(read_timeout=3600)
     print(file)
     old_name = file.file_path
-    new_name = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name
-    new_name_esc = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name.replace(" ", "\\ ") 
-    os.rename(old_name, new_name)
-    bashCommand = f"unrar x {new_name_esc}"
-    print(bashCommand)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print(output)
-    print(error)
-    
+    new_name = os.path.dirname(os.path.abspath(old_name)) + "/" + msg.document.file_name.replace(" ", "_") 
+    x = re.search(r"part([0-9])*.rar|zip.([0-9])*$", txt)
+
+    if (not x) or (x and x.group(1) == "1"):
+        #os.rename(old_name, new_name)
+        bashCommand = f"atool -x {new_name}"
+        print(bashCommand)
+
+        x = re.search(r"(.*)\.(?:part|zip.)(([0-9])*)(?:.rar$|$)", new_name)
+
+        if x.group(1) in volumes:
+            volumes[x.group(1)][x.group(3)] = new_name       
+        else:
+            volumes[x.group(1)] = {x.group(3):new_name}
+              
+        if "1" in volumes[x.group(1)]:
+            print("extract")
+        else:
+            print("wait")
+
+        #process = subprocess.Popen(['unrar','x',new_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #output, error = process.communicate()
+        #print(output)
+        #print(error)
+""" 
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
 
@@ -55,12 +74,12 @@ def main():
     # updater = Updater(keys.API_KEY, use_context=True, base_url="http://192.168.43.53:8081/bot")
     # updater.bot.logOut();
 
-    # application.add_handler(CommandHandler("start", start))
-    # application.add_handler(CommandHandler("help", help_command))
-    # application.add_handler(CommandHandler("addMe", addReceiver_command))
-    # application.add_handler(CommandHandler("getReceivers", getReceivers_command))
-    # application.add_handler(MessageHandler(Filters.all, handle_message))
-    application.add_handler(MessageHandler(filters.Document.ALL, handle_documents))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("addMe", addReceiver_command))
+    application.add_handler(CommandHandler("getReceivers", getReceivers_command))
+    application.add_handler(MessageHandler(filters.Document.ALL, handle_message))
+    #application.add_handler(MessageHandler(filters.Document.ALL, handle_documents))
     application.add_error_handler(error)
 
     application.run_polling()
